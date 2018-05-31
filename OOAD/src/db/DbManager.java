3250ce6977f3dc5c03a26fc7a937c2ac940bdd9e
keya <keya@10.21.16.171>
2018-05-31 14:36:08 -0500

    first commit

---
+++ OOAD/src/db/DbManager.java
@@ -0,0 +1,27 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ * 
+ * @author mehra
+ * getConnection method help us to connect to the appropriate database. In this project we only have one
+ * database. Data comes from MyDB interface.
+ */
+public class DbManager implements MyDB{
+
+	public Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection myConnection = DriverManager.getConnection(CONN_URL,USER,PASS);
+			return myConnection;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public static void main(String[] args) {
+		DbManager dbManager=new DbManager();
+		dbManager.getConnection();
+	}
+}
